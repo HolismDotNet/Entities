bc3a05cb6e5f99a15becb95f716ad74a41ef85cd
Saeed Nemati <sn.1361@gmail.com>
2022-01-22 22:43:48 +0330

    committed by script

--- Business/EntityTypesBusiness.cs
+++ Business/EntityTypesBusiness.cs
@@ -1,76 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Holism.Business;
-using Holism.DataAccess;
-using Holism.Entities.DataAccess;
-using Holism.Entities.Models;
-using Holism.Infra;
+﻿namespace Entities;
 
-namespace Holism.Entities.Business
+public class EntityTypeBusiness : Business<EntityType, EntityType>
 {
-    public class EntityTypeBusiness : Business<EntityType, EntityType>
+    protected override Repository<EntityType> WriteRepository =>
+        Repository.EntityType;
+
+    protected override ReadRepository<EntityType> ReadRepository =>
+        Repository.EntityType;
+
+    private static Dictionary<string, Guid> entityTypeNames;
+
+    private static Dictionary<Guid, string> entityTypeGuids;
+
+    private static Dictionary<string, Guid> EntityTypeNames
     {
-        protected override Repository<EntityType> WriteRepository =>
-            Repository.EntityType;
-
-        protected override ReadRepository<EntityType> ReadRepository =>
-            Repository.EntityType;
-
-        private static Dictionary<string, Guid> entityTypeNames;
-
-        private static Dictionary<Guid, string> entityTypeGuids;
-
-        private static Dictionary<string, Guid> EntityTypeNames
+        get
         {
-            get
+            if (entityTypeNames == null)
             {
-                if (entityTypeNames == null)
-                {
-                    entityTypeNames =
-                        new EntityTypeBusiness()
-                            .GetAll()
-                            .ToDictionary(i => i.Name.ToLower(), i => i.Guid);
-                }
-                return entityTypeNames;
+                entityTypeNames =
+                    new EntityTypeBusiness()
+                        .GetAll()
+                        .ToDictionary(i => i.Name.ToLower(), i => i.Guid);
             }
+            return entityTypeNames;
         }
+    }
 
-        private static Dictionary<Guid, string> EntityTypeGuids
+    private static Dictionary<Guid, string> EntityTypeGuids
+    {
+        get
         {
-            get
+            if (entityTypeGuids == null)
             {
-                if (entityTypeGuids == null)
-                {
-                    entityTypeGuids =
-                        new EntityTypeBusiness()
-                            .GetAll()
-                            .ToDictionary(i => i.Guid, i => i.Name.ToLower());
-                }
-                return entityTypeGuids;
+                entityTypeGuids =
+                    new EntityTypeBusiness()
+                        .GetAll()
+                        .ToDictionary(i => i.Guid, i => i.Name.ToLower());
             }
+            return entityTypeGuids;
         }
+    }
 
-        public Guid GetGuid(string name)
+    public Guid GetGuid(string name)
+    {
+        name = name.ToLower();
+        if (EntityTypeNames.ContainsKey(name))
         {
-            name = name.ToLower();
-            if (EntityTypeNames.ContainsKey(name))
-            {
-                return EntityTypeNames[name];
-            }
-            throw new ServerException(@$"Entities {
-                    name} is not specified in the database.");
+            return EntityTypeNames[name];
         }
+        throw new ServerException(@$"Entities {
+                name} is not specified in the database.");
+    }
 
-        public string GetName(Guid guid)
+    public string GetName(Guid guid)
+    {
+        if (EntityTypeGuids.ContainsKey(guid))
         {
-            if (EntityTypeGuids.ContainsKey(guid))
-            {
-                return EntityTypeGuids[guid];
-            }
-            throw new ServerException(@$"No Entities is defined to have the guid {
-                    guid}");
+            return EntityTypeGuids[guid];
         }
+        throw new ServerException(@$"No Entities is defined to have the guid {
+                guid}");
     }
 }
